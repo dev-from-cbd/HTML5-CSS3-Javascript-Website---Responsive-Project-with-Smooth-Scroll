b1cd5ad6c3e4def196333a093736ed3c6ec07b02
veryfatwombat <veryfatwombat@gmail.com>
2020-12-13 00:12:25 +1100

    js

--- app.js
+++ app.js
@@ -2,5 +2,8 @@ const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
 
 const mobileMenu = () => {
-    menu.classList.toggle
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle('active')
 }
+
+menu.addEventListener('click', mobileMenu)
