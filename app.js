399fd3e1badc2937f5c334f4e5c23fd16d925f94
veryfatwombat <veryfatwombat@gmail.com>
2020-12-22 08:29:18 +1100

    js+css3

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar__menu')
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo');
 
 const mobileMenu = () => {
-    menu.classList.toggle('is-active')
-    menuLinks.classList.toggle('active')
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
 }
 
-menu.addEventListener('click', mobileMenu)
+menu.addEventListener('click', mobileMenu);
+
+const highlightMenu = () => {
+    const elem = document.querySelector('.highlight');
+    const homeMenu = document.querySelector('#home-page');
+    const aboutMenu = document.querySelector('#about-page');
+    const servicesMenu = document.querySelector('.#services-page');
+}
