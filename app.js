637fb73b9fc838a0fa6f5e260e33b85f313c171b
veryfatwombat <veryfatwombat@gmail.com>
2020-12-23 12:05:47 +1100

    js

--- app.js
+++ app.js
@@ -10,8 +10,28 @@ const mobileMenu = () => {
 menu.addEventListener('click', mobileMenu);
 
 const highlightMenu = () => {
-    const elem = document.querySelector('.highlight');
-    const homeMenu = document.querySelector('#home-page');
-    const aboutMenu = document.querySelector('#about-page');
-    const servicesMenu = document.querySelector('.#services-page');
+    const elem = document.querySelector('.highlight')
+    const homeMenu = document.querySelector('#home-page')
+    const aboutMenu = document.querySelector('#about-page')
+    const servicesMenu = document.querySelector('#services-page')
+    let scrollPos = window.scrollY
+
+    if(window.innerWidth > 960 && scrollPos < 600) {
+        homeMenu.classList.add('highlight')
+        aboutMenu.classList.remove('highlight')
+        return
+    } else if (window.innerWidth > 960 && scrollPos < 1400) {
+        aboutMenu.classList.add('highlight')
+        homeMenu.classList.remove('highlight')
+        servicesMenu.classList.remove('highlight')
+        return
+    } else if (window.innerWidth > 960 && scrollPos < 2345) {
+        servicesMenu.classList.remove('highlight')
+        aboutMenu.classList.remove('highlight')
+        return
+    }
+
+    if((elem && window.innerWidth < 960 && scrollPos < 600) || elem) {
+        elem.classList.remove('highlight')
+    }
 }
