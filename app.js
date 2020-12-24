d11e2d23de113ce9ee5d4679f23413511a1fa504
veryfatwombat <veryfatwombat@gmail.com>
2020-12-25 00:00:25 +1100

    js

--- app.js
+++ app.js
@@ -10,31 +10,43 @@ const mobileMenu = () => {
 menu.addEventListener('click', mobileMenu);
 
 const highlightMenu = () => {
-    const elem = document.querySelector('.highlight')
-    const homeMenu = document.querySelector('#home-page')
-    const aboutMenu = document.querySelector('#about-page')
-    const servicesMenu = document.querySelector('#services-page')
+    const elem = document.querySelector('.highlight');
+    const homeMenu = document.querySelector('#home-page');
+    const aboutMenu = document.querySelector('#about-page');
+    const servicesMenu = document.querySelector('#services-page');
     let scrollPos = window.scrollY
+    // console.log(scrollPos);
 
     if(window.innerWidth > 960 && scrollPos < 600) {
-        homeMenu.classList.add('highlight')
-        aboutMenu.classList.remove('highlight')
+        homeMenu.classList.add('highlight');
+        aboutMenu.classList.remove('highlight');
         return
     } else if (window.innerWidth > 960 && scrollPos < 1400) {
-        aboutMenu.classList.add('highlight')
-        homeMenu.classList.remove('highlight')
-        servicesMenu.classList.remove('highlight')
+        aboutMenu.classList.add('highlight');
+        homeMenu.classList.remove('highlight');
+        servicesMenu.classList.remove('highlight');
         return
     } else if (window.innerWidth > 960 && scrollPos < 2345) {
-        servicesMenu.classList.remove('highlight')
-        aboutMenu.classList.remove('highlight')
+        servicesMenu.classList.remove('highlight');
+        aboutMenu.classList.remove('highlight');
         return
     }
 
     if((elem && window.innerWidth < 960 && scrollPos < 600) || elem) {
-        elem.classList.remove('highlight')
+        elem.classList.remove('highlight');
     }
 }
 
-window.addEventListener('scroll', highlightMenu)
-window.addEventListener('click', highlightMenu)
+window.addEventListener('scroll', highlightMenu);
+window.addEventListener('click', highlightMenu);
+
+const hideMobileMenu = () => {
+    const menuBars = document.querySelector('.is-active')
+    if(window.innerWidth <= 768 && menuBars) {
+        menu.classList.toggle('is-active')
+        menuLinks.classList.remove('active')
+    }
+}
+
+menuLinks.addEventListener('click', hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);
